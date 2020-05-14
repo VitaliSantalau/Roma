6f1bfc8686a373edf8e854afc20a3fd443e61e3f
VitaliSantalau <v.santalau@gmail.com>
2020-05-14 18:06:20 +0300

    finish

--- romasantalau/scripts/script.js
+++ romasantalau/scripts/script.js
@@ -1,29 +1,30 @@
 // info about parents
 let parents = document.querySelector('.parents');
+let fuck = document.querySelector('.fuck');
 let parentsInfo = document.querySelector('.parents_info');
 
 parents.onclick = function(e) {
   if (!e.target.classList.contains('open')) {
-    /*parentsInfo.style.transform = 'translateY(0)';*/
+    parentsInfo.style.transform = 'translateY(0)';
     parents.classList.add('open');
   } else {
-    /*parentsInfo.style.transform = 'translateY(-15.5em)';*/
+    parentsInfo.style.transform = 'translateY(-15.5em)';
     parents.classList.remove('open');
 
   }
 }
 
 // info about teacher
-let teacher = document.querySelector('.teacher');
-let teacherInfo = document.querySelector('.teacher_info');
+//let teacher = document.querySelector('.teacher');
+//let teacherInfo = document.querySelector('.teacher_info');
 
-teacher.onclick = function(e) {
-  if (!e.target.classList.contains('open')) {
-    /*teacherInfo.style.transform = 'translateY(0)';*/
-    teacher.classList.add('open');
-  } else {
-    /*teacherInfo.style.transform = 'translateY(-13.5em)';*/
-    teacher.classList.remove('open');
+//teacher.onclick = function(e) {
+//  if (!e.target.classList.contains('open')) {
+//    teacherInfo.style.transform = 'translateY(0)';
+//    teacher.classList.add('open');
+//  } else {
+//    teacherInfo.style.transform = 'translateY(-13.5em)';
+//    teacher.classList.remove('open');
+//  }
+//}
 
-  }
-}
